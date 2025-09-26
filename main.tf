bc6bc0259172adc4980862364b6d738b273e1ee1
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-09-25 20:39:04 -0400

    Update main.tf
    added some more lines,

--- main.tf
+++ main.tf
@@ -25,3 +25,46 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-secure-vm-${random_id.suffix.hex}"
   location = var.location
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-secure"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Subnet (segment of the network)
+resource "azurerm_subnet" "internal" {
+  name                 = "subnet-internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Public IP (so you can SSH from internet)
+resource "azurerm_public_ip" "main" {
+  name                = "pip-secure-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
+
+# Network Security Group (firewall)
+resource "azurerm_network_security_group" "main" {
+  name                = "nsg-secure-vm"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  # Allow SSH only
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
