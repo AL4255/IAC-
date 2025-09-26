d7987e19273644328ec49115e019c11a23d7fcc5
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-09-26 16:31:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,3 +68,16 @@ resource "azurerm_network_security_group" "main" {
     destination_address_prefix = "*"
   }
 }
+
+resource "azurerm_network_interface" "main" {
+  name                = "nic-secure-vm"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
