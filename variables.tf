2d556524321b053d1d02e07756a622e1d7ea1e40
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-09-29 06:06:41 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,28 @@
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+
+variable "admin_username" {
+  description = "VM amin username"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+  default     = "~/.sshid_rsa.pub"
+}
+
+
+variable "vm_size" {
+  description = "VM size"
+  type        = string
+  default     = "Standard_B1s"
+}
+
 # this the varibles for main.tf
