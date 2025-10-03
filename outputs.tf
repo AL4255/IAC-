09b6578902b627460c0c58bc665a00b0c4b249c9
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-10-03 12:49:39 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,22 @@
 #this tell my terrafomr to outoput the results to my terminal 
+output "resouce_group_nam" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+
+output "vm_name" {
+  description = "Name of the virtual machine"
+  value       = azurerm_linux_virtual_machine.main.name
+}
+
+
+output "public_ip_address" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.main.ip_address
+}
+
+output "ssh_connection" {
+  description = "SSH connection command"
+  value       = "ssh ${var.admin_username}@{azurerm_public_ip.mainip_address}"
+}
