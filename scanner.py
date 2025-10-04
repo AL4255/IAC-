2db0f338145869684f5272718dee48b3e3e4e592
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-10-03 20:06:53 -0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,30 @@
+#1/bin/env python3 
+
+
+"""
+    Secuirty scanner to validate VM secuirty compliance 
+"""
+
+
+import sys 
+import subprocess
+
+
+from aazure.identity import DefaultAzureCredential
+
+from azure.mgmt.compute import ComputeManagemetClient 
+
+
+class VMSecurityScanner:
+    def __init__(self, subscription_id):
+        self.subscription_id = subscription_id
+        self.crednetial = DefaultAzureCrednetial()
+        sefl.compute_client = ComputeManagementClient(
+            self.credential,
+            subscription_id
+        )
+
+
+    def scan_vm_security(self, resource_group, vm_name):
+        results = scanner.scan_vm_security("rg-name", 'vm0name')
 
