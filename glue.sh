fa896a99dbcae7f01f98696a96bcbd701a93c00f
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-10-08 19:04:32 -0400

    Update glue.sh

--- glue.sh
+++ glue.sh
@@ -1,1 +1,10 @@
 # this will a simple bash file that is running the commands for whole procss
+
+
+#!/bin/bash
+     set -e
+     echo "Running Terraform apply..."
+     terraform apply -auto-approve
+     echo "Terraform apply completed successfully!"
+     echo "Running scanner.py..."
+     python scanner.py
